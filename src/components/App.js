77ab7fa60eab0f03f0fc9d0014b433bbcd31fb21
jemojeremmy2@gmail.com <jemojeremmy2>
2024-05-03 12:18:06 +0300

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
-
+import axios from "axios";
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys,setToys]=useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:3001/toys')
+    .then((response)=>setToys(response.data))
+  },[])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
-
+  function addToy(newToy){
+    setToys([...toys,newToy])
+  }
+  function deleteToy(trashToy){
+    const updatedToys=toys.filter((toy)=>toy.id!=trashToy.id)
+    setToys(updatedToys)
+  }
+  function updateToy(updatedToy) {
+    const updatedToys = toys.map((toy) =>
+      toy.id === updatedToy.id ? updatedToy : toy
+    );
+    setToys(updatedToys);
+  }
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm addToy={addToy}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys}
+      deleteToy={deleteToy}
+      updatedToy={updateToy}
+      />
     </>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys,deleteToy,updateToy}) {
+  const toyCards=toys.map((toy)=>(
+    <ToyCard key={toy.id}
+    toy={toy}
+    deleteToy={deleteToy}
+    updateToy={updateToy}
+    />
+  ))
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toyCards}</div>
   );
 }
 
